134c4276f4dae8684a4488c4523457c915279519
PingPig <35329906+PingPig@users.noreply.github.com>
2020-02-27 17:25:37 +0800

    Update shodan_search.py

--- python/ipsearch/shodanApi/shodan_search.py
+++ python/ipsearch/shodanApi/shodan_search.py
@@ -1,5 +1,5 @@
 import shodan
-api = shodan.Shodan("QsLloESzY3u7gCtCtxv0irZZyCbQayZN")
+api = shodan.Shodan("xxxxxxx")
 results = api.search('cisco')
 for result in results['matches']:
     iplist = result['ip_str']+":"+ str(result['port'])
